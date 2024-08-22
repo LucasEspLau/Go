d81300ba8624880fb3f6247016edc4c2ba2c3baa
ALP300 <aitorpalomino1@gmail.com>
2024-08-22 10:11:47 -0500

    realizar pantalla inicial

--- app/(tabs)/main.tsx
+++ app/(tabs)/main.tsx
@@ -1,44 +1,111 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Alert, ImageBackground } from 'react-native';
-import { useNavigation } from 'expo-router';
+import React from 'react';
+import { View, Text, ImageBackground, TouchableOpacity, StyleSheet, Button } from 'react-native';
+import { useRouter } from 'expo-router';
 
 export default function MainScreen() {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const navigation = useNavigation();
+  const router = useRouter();
 
   const handleRegister = () => {
+    router.push('/register'); // Navega a la pantalla de registro
+  };
 
-    Alert.alert('Registro exitoso', `Bienvenido, ${username}!`);
-    navigation.navigate('register' as never);
-
+  const handleLogin = () => {
+    router.push('/login'); // Navega a la pantalla de inicio de sesión
   };
 
   return (
-    <ImageBackground 
-      source={require('../../assets/images/mainbg.png')}
-    style={styles.container}
-    resizeMode='stretch'
-    >
-      <Text style={styles.customFontText}>
-        Welcome to Home Screen
-      </Text>
-      <Button title='Ir Registro' onPress={handleRegister}></Button>
-    </ImageBackground>
+    <View style={styles.container}>
+      <ImageBackground 
+        source={require('../../assets/images/mainbg.jpg')}
+        style={styles.backgroundImage}
+      >
+        <Text style={styles.mainText}>
+          Todo lo que necesitas, a un toque de distancia.
+        </Text>
+        <Text style={styles.subText}>
+          ¡REGÍSTRATE HOY!
+        </Text>
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.registerButton} onPress={handleRegister}>
+            <Text style={styles.registerButtonText}>Regístrate</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+            <Text style={styles.loginButtonText}>Inicia Sesión</Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'rgba(245, 245, 245, 1)',
+  },
+  backgroundImage: {
+    width: '100%',
+    height: '100%',
     justifyContent: 'center',
     alignItems: 'center',
-
   },
-  customFontText: {
-    fontFamily: 'MoonGet', // Nombre de la fuente personalizada
-    fontSize: 24,
-    color: '#333',
+  mainText: {
+    width: 309,
+    color: 'white',
+    fontFamily: 'Inter',
+    fontWeight: '800',
+    fontSize: 48,
+    textAlign: 'left',
+    position: 'absolute',
+    top: 78,
+    left: 17,
+  },
+  subText: {
+    width: 282,
+    color: 'white',
+    fontFamily: 'Inter',
+    fontWeight: '800',
+    fontSize: 20,
+    textAlign: 'left',
+    position: 'absolute',
+    top: 328,
+    left: 20,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    top: 480, // Ajusta esta posición para mover los botones más arriba
+    left: 0, // Centra horizontalmente al utilizar alignItems: 'center'
+    right: 0, // Centra horizontalmente al utilizar alignItems: 'center'
+    alignItems: 'center', // Centra los botones en el eje horizontal
+  },
+  registerButton: {
+    width: 303,
+    height: 63,
+    backgroundColor: 'rgba(255, 127, 66, 1)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    marginBottom: 30,
+  },
+  registerButtonText: {
+    color: 'white',
+    fontFamily: 'Inter',
+    fontWeight: '800',
+    fontSize: 20,
+  },
+  loginButton: {
+    width: 303,
+    height: 63,
+    backgroundColor: 'rgba(23, 21, 21, 1)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+  },
+  loginButtonText: {
+    color: 'white',
+    fontFamily: 'Inter',
+    fontWeight: '800',
+    fontSize: 20,
   },
 });
